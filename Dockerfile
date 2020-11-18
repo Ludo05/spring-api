3f98b1b3a1992020cba431dbc4b2a49b24ffd6ef
lewis150193 <35262933+lewis150193@users.noreply.github.com>
2020-11-18 21:50:08 +0000

    Using request headers (#4)

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
 FROM openjdk:latest
+#FROM gradle:latest
+#CMD "chmod +x gradlew"
+#RUN "./gradlew build"
 ARG JAR_FILE=./build/libs/my-security-site-0.1.0.jar
 COPY ${JAR_FILE} app.jar
 EXPOSE 9090/tcp
--- src/main/java/com/example/security/security/controller/HomeController.java
+++ src/main/java/com/example/security/security/controller/HomeController.java
@@ -1,13 +1,31 @@
 package com.example.security.security.controller;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @RestController
 public class HomeController {
 
     @GetMapping(path = "/")
-    public String getStudent() {
-        return "Welcome to my security api v4!!!";
+    public ResponseEntity<String> getStudent(@RequestHeader(value = "Connection") String authorization,
+                                             HttpServletRequest request) {
+
+        String requestResponse = request.getHeader("X-Header");
+        if (requestResponse == null) {
+            requestResponse = UUID.randomUUID().toString();
+        }
+        //Will return the new header back to the user or frontend for use
+        HttpHeaders response = new HttpHeaders();
+        response.set("X-Test-Header", requestResponse);
+
+
+        return new ResponseEntity<>(authorization, response, HttpStatus.OK);
     }
 }
